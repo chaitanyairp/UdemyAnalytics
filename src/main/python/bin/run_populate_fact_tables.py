6dbf21fa97144884147bd31bb9526af00d100a28
Chaitanya <chaitanya.ivaturi41@gmail.com>
2023-01-20 00:50:41 +0530

    Added fact campaign details

--- src/main/python/bin/run_populate_fact_tables.py
+++ src/main/python/bin/run_populate_fact_tables.py
@@ -11,18 +11,17 @@ def populate_fact_tables(spark, user_event_df):
         .load()
 
     campaign_dim_df = campaign_dim_df.select(
-        "campaign_key",
-        "campaign_id",
-        "course_campaign_name",
+        f.col("campaign_key"),
+        f.col("campaign_id").alias("campaign_dim_id"),
+        f.col("course_campaign_name").alias("campaign_dim_name"),
         "campaign_start_date",
         "campaign_end_date"
     )
-    campaign_dim_df = campaign_dim_df.alias("campaign_dim_df")
-    user_event_df = user_event_df.alias("user_event_df")
 
-    user_campaign_join_cond = ((f.col("campaign_dim_df.campaign_id") == f.col("user_event_df.campaign_id"))
-                               & (f.col("campaign_dim_df.course_campaign_name") == f.col("user_event_df.course_campaign_name")))
+    user_campaign_join_cond = ((f.col("campaign_id") == f.col("campaign_dim_id"))
+                               & (f.col("course_campaign_name") == f.col("campaign_dim_name")))
     user_event_df = user_event_df.join(campaign_dim_df, user_campaign_join_cond).drop(
-        *[campaign_dim_df.campaign_id,campaign_dim_df.course_campaign_name])
-
+        "campaign_dim_id",
+        "campaign_dim_name"
+    )
     user_event_df.show(truncate=False)
